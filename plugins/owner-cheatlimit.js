2f25bb7487b8bc80556cd99e7a92a4106a0d2d4c
Hendralubeck-88 <118050450+Hendralubeck-88@users.noreply.github.com>
2022-12-26 00:18:43 +0700

    Update owner-cheatlimit.js

--- plugins/owner-cheatlimit.js
+++ plugins/owner-cheatlimit.js
@@ -1,16 +1,29 @@
 let handler = async (m, { conn }) => {
+
     let user = global.db.data.users[m.sender]
+
         conn.reply(m.chat, `*Succes Cheat !*`, m)
-        global.db.data.users[m.sender].money = 999999999999999999999999999999999999999999999999999999999999999999999999999999999999
-        global.db.data.users[m.sender].limit = 999999999999999999999999999999999999999999999999999999999999999999999999999999999999
-        global.db.data.users[m.sender].level = 999999999999999999999999999999999999999999999999999999999999999999999999999999999999
-        global.db.data.users[m.sender].exp = 999999999999999999999999999999999999999999999999999999999999999999999999999999999999
-        global.db.data.users[m.sender].diamond = 999999999999999999999999999999999999999999999999999999999999999999999999999999999999
-        global.db.data.users[m.sender].pet = 999999999999999999999999999999999999999999999999999999999999999999999999999999999999
-        global.db.data.users[m.sender].gold =999999999999999999999999999999999999999999999999999999999999999999999999999999999999
-        global.db.data.users[m.sender].emerald =999999999999999999999999999999999999999999999999999999999999999999999999999999999999
-        global.db.data.users[m.sender].legendary =999999999999999999999999999999999999999999999999999999999999999999999999999999999999
+
+        global.db.data.users[m.sender].money = 999999999
+
+        global.db.data.users[m.sender].limit = 9999
+
+        global.db.data.users[m.sender].level = 88888
+
+        global.db.data.users[m.sender].exp = 98888888
+
+        global.db.data.users[m.sender].diamond = 98888
+
+        global.db.data.users[m.sender].pet = 888888
+
+        global.db.data.users[m.sender].gold =989999
+
+        global.db.data.users[m.sender].emerald =99999
+
+        global.db.data.users[m.sender].legendary =9888889
+
 }
+
 handler.command = /^(curang)$/i
 
 handler.mods = true
