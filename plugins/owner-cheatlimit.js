8bcae9298dae0f1bd0c83453e2793ac22a11f69a
Hendralubeck-88 <118050450+Hendralubeck-88@users.noreply.github.com>
2022-12-26 07:24:48 +0700

    Update owner-cheatlimit.js

--- plugins/owner-cheatlimit.js
+++ plugins/owner-cheatlimit.js
@@ -4,23 +4,23 @@ let handler = async (m, { conn }) => {
 
         conn.reply(m.chat, `*Succes Cheat !*`, m)
 
-        global.db.data.users[m.sender].money = 999999999
+        global.db.data.users[m.sender].money = 999999
 
-        global.db.data.users[m.sender].limit = 9999
+        global.db.data.users[m.sender].limit = 9
 
-        global.db.data.users[m.sender].level = 88888
+        global.db.data.users[m.sender].level = 1
 
-        global.db.data.users[m.sender].exp = 98888888
+        global.db.data.users[m.sender].exp = 2000
 
-        global.db.data.users[m.sender].diamond = 98888
+        global.db.data.users[m.sender].diamond = 555
 
-        global.db.data.users[m.sender].pet = 888888
+        global.db.data.users[m.sender].pet = 55
 
-        global.db.data.users[m.sender].gold =989999
+        global.db.data.users[m.sender].gold =555
 
-        global.db.data.users[m.sender].emerald =99999
+        global.db.data.users[m.sender].emerald =55
 
-        global.db.data.users[m.sender].legendary =9888889
+        global.db.data.users[m.sender].legendary =99
 
 }
 
